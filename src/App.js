ec0e793a0a5e2088b1ce57e22219ef89ef305db6
Angelo Bennett <angelobennett86@gmail.com>
2021-10-04 00:27:47 -0400

    routing and post form

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+const App = () => {
+	return (
+		<div className="container p-5">
+			<h1>Bee Well 🐝</h1>
+		</div>
+	);
+};
+
+export default App;
