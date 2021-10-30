9b722dc251cd8d25c573b36cb10e84efb3ac3449
Angelo Bennett <angelobennett86@gmail.com>
2021-10-29 21:06:05 -0400

    nav & react-start edit

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -6,15 +6,15 @@ const Nav = ({history}) => (
     <nav>
         <ul className="nav nav-tabs">
             <li className="nav-item pr-3 pt-3 pb-3">
-                <Link to="/">Home</Link>
+                <Link to="/" style={{'marginRight':'10px'}}>Home</Link>
             </li>
              <li className="nav-item pr-3 pt-3 pb-3">
-                <Link to="/create">Create</Link>
+                <Link to="/create" style={{'marginRight':'10px'}}>Create</Link>
             </li>
              {
                  !getUser() && (
                      <li className="nav-item ml-auto pr-3 pt-3 pb-3">
-                         <Link to="/login">Login</Link>
+                         <Link to="/login" style={{'marginRight':'10px'}}>Login</Link>
                          </li>
                  )}
              {
